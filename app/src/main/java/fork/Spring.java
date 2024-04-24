f439e0b1dd527962e9ae963529aa1520ce2fd8c4
unknown <daichi1616@outlook.jp>
2024-04-24 13:41:39 +0900

    first commit

---
+++ app/src/main/java/fork/Spring.java
@@ -0,0 +1,11 @@
+package fork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring {
+    public void spring(String[] args) {
+        SpringApplication.run(Spring.class, args);
+    }
+}
